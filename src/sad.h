9e788efbd5cd4b6f581c417b5da95e6e24c08d32
Arch Strange <arch.strange@foxmail.com>
2020-01-05 19:10:05 +0800

    Array, Dict: better memory magaging.

--- src/sad.h
+++ src/sad.h
@@ -35,6 +35,18 @@ struct Sad {
 	};
 };
 
+static inline struct Sad SAD_DEFAULT_VALUE();
+static inline struct Sad SAD_STRING_VALUE(Str s);
+static inline struct Sad SAD_ARRAY_VALUE(SadArray a);
+static inline struct Sad SAD_DICT_VALUE(SadDict d);
+static inline struct Sad Sad_clone(struct Sad src);
+static inline void Sad_copy(struct Sad self, struct Sad src);
+static inline void Sad_free(struct Sad self);
+
+///////////////////////////////////////////////////////////////////////////////
+// inline methods impl
+///////////////////////////////////////////////////////////////////////////////
+
 static inline struct Sad SAD_DEFAULT_VALUE()
 {
 	return (struct Sad){ .type = SAD_INVALID };
@@ -53,6 +65,43 @@ static inline struct Sad SAD_ARRAY_VALUE(SadArray a)
 static inline struct Sad SAD_DICT_VALUE(SadDict d)
 {
 	return (struct Sad){ .type = SAD_DICT, .dict = d };
+}
+
+static inline struct Sad Sad_clone(struct Sad src)
+{
+	struct Sad self = SAD_DEFAULT_VALUE();
+	switch (src.type) {
+	case SAD_STRING:
+		self.str = Str_clone(src.str);
+		break;
+	case SAD_ARRAY:
+		self.array = SadArray_clone(src.array);
+		break;
+	case SAD_DICT:
+		self.dict = SadDict_clone(src.dict);
+		break;
+	}
+	return self;
+}
+
+static inline void Sad_copy(struct Sad self, struct Sad src)
+{
+	if (self.type != src.type) {
+		Sad_free(self);
+		self = Sad_clone(src);
+		return;
+	}
+	switch (src.type) {
+	case SAD_STRING:
+		Str_copy(self.str, src.str);
+		break;
+	case SAD_ARRAY:
+		SadArray_copy(self.array, src.array);
+		break;
+	case SAD_DICT:
+		SadDict_copy(self.dict, src.dict);
+		break;
+	}
 }
 
 static inline void Sad_free(struct Sad self)
